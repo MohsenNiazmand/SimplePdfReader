10186371dc4eb20d3e1a541395e926570e56f3b6
Mohsen Niazmand <it.mohsen.niazmand@gmail.com>
2021-01-21 12:19:22 +0330

    Give Permission Access to Storage

--- app/src/main/java/com/example/simplepdfreader/MainActivity.java
+++ app/src/main/java/com/example/simplepdfreader/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.simplepdfreader;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_PERMISSION =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permission();
+
+    }
+
+    private void permission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                Toast.makeText(this,"Please Grant Permission",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_PERMISSION);
+            }
+        }
+        else {
+            Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+//            initViews();
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION)
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                initViews();
+            }else {
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+
+            }
     }
 }
