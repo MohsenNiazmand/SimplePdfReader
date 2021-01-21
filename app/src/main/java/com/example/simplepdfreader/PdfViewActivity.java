6faa8ee58950ac1b07176bdd8b25824a47103787
Mohsen Niazmand <it.mohsen.niazmand@gmail.com>
2021-01-21 12:26:41 +0330

    perform view pdf file

--- app/src/main/java/com/example/simplepdfreader/PdfViewActivity.java
+++ app/src/main/java/com/example/simplepdfreader/PdfViewActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnPageScrollListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+
 public class PdfViewActivity extends AppCompatActivity {
 
+    PDFView pdfView;
+    private int position=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_view);
+        pdfView=findViewById(R.id.pdfView);
+        position = getIntent().getIntExtra("position",-1);
+        viewPdf();
+    }
+
+    private void viewPdf() {
+        pdfView.fromFile(MainActivity.mFiles.get(position))
+                .enableSwipe(true)
+                .enableAnnotationRendering(true)
+                .onPageScroll(new OnPageScrollListener() {
+                    @Override
+                    public void onPageScrolled(int page, float positionOffset) {
+
+                    }
+                })
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
     }
 }
