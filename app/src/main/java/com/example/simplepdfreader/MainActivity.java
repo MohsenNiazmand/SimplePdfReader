f8a85f440e53e1e50620d466dc26a9a1d7de452c
Mohsen Niazmand <it.mohsen.niazmand@gmail.com>
2021-01-21 12:22:37 +0330

    Setup RecyclerView and Get Pdf Files

--- app/src/main/java/com/example/simplepdfreader/MainActivity.java
+++ app/src/main/java/com/example/simplepdfreader/MainActivity.java
@@ -4,19 +4,30 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_PERMISSION =1;
+    public static ArrayList<File> mFiles = new ArrayList<>();
+    RecyclerView recyclerView;
+    private File folder;
+    String[] items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.pdfs_rv);
         permission();
 
     }
@@ -35,10 +46,44 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
-//            initViews();
+            initViews();
 
         }
 
+    }
+
+    private void initViews() {
+        folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        mFiles = getPdfFiles(folder);
+
+        ArrayList<File> myPdf = getPdfFiles(Environment.getExternalStorageDirectory());
+        items = new String[myPdf.size()];
+        for (int i=0 ; i< items.length ;i++){
+            items[i] = myPdf.get(i).getName().replace("pdf","");
+        }
+        PdfAdapter pdfAdapter = new PdfAdapter(this, mFiles,items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
+        recyclerView.setAdapter(pdfAdapter);
+
+
+    }
+
+    private ArrayList<File> getPdfFiles(File folder) {
+        ArrayList<File> arrayList = new ArrayList<>();
+        File[] files = folder.listFiles();
+        if (files != null){
+            for (File singleFile : files){
+                if (singleFile.isDirectory() && !singleFile.isHidden()){
+                    arrayList.addAll(getPdfFiles(singleFile));
+                }
+                else {
+                    if (singleFile.getName().endsWith(".pdf")){
+                        arrayList.add(singleFile);
+                    }
+                }
+            }
+        }
+        return arrayList;
     }
 
     @Override
@@ -46,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSION)
             if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                initViews();
+                initViews();
             }else {
                 Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/example/simplepdfreader/PdfAdapter.java
+++ app/src/main/java/com/example/simplepdfreader/PdfAdapter.java
@@ -1,4 +1,74 @@
 package com.example.simplepdfreader;
 
-public class PdfAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PdfAdapter extends RecyclerView.Adapter<PdfAdapter.PdfViewHolder> {
+
+    private Context mContext;
+    private ArrayList<File> mFiles;
+    String[] items;
+
+    public PdfAdapter(Context mContext, ArrayList<File> mFiles, String[] items) {
+        this.mContext = mContext;
+        this.mFiles = mFiles;
+        this.items = items;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public PdfViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_pdf,parent,false );
+
+        return new PdfViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull PdfAdapter.PdfViewHolder holder, int position) {
+        holder.file_tv.setText(items[position]);
+        holder.pdf_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(mContext,PdfViewActivity.class);
+                intent.putExtra("position",position);
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFiles.size();
+    }
+
+    public class PdfViewHolder extends RecyclerView.ViewHolder{
+        private TextView file_tv;
+        private ImageView file_icon;
+        private LinearLayout pdf_item;
+
+        public PdfViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            file_tv = itemView.findViewById(R.id.tv_pdf_name);
+            file_icon = itemView.findViewById(R.id.img_icon);
+            pdf_item = itemView.findViewById(R.id.item_pdf);
+        }
+
+
+    }
+
 }
